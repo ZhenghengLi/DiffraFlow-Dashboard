b51e8a66d876e6e34df9efdc48cebcf89ce9831a
Zhengheng Li <zhenghenge@gmail.com>
2020-10-16 11:04:02 +0800

    metrics service: set up interval routine

--- src/app/metrics/metrics-data.service.ts
+++ src/app/metrics/metrics-data.service.ts
@@ -1,25 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { interval, Subscription } from 'rxjs';
 
 @Injectable()
 export class MetricsDataService {
-    constructor() {}
+    constructor(private http: HttpClient) {}
 
-    private myIdVal: number = 0;
+    private _configUrl: string = 'assets/config.json';
+    private _myIdVal: number = 0;
+    private _aggregatorAddress: string = null;
+
+    private _intervalSubscription: Subscription = null;
 
     log(msg: string): void {
         console.log('metrics-data: ', msg);
     }
 
     get myId(): number {
-        return this.myIdVal;
+        return this._myIdVal;
     }
 
     start(): void {
-        this.myIdVal = Math.random();
-        console.log('start metrics-data service:', this.myIdVal);
+        this._myIdVal = Math.random();
+        console.log('start metrics-data service:', this._myIdVal);
+        this.http.get(this._configUrl).subscribe((data) => {
+            if ('aggregator_address' in data) {
+                this._aggregatorAddress = data['aggregator_address'];
+                console.log('aggregator_address = ', this._aggregatorAddress);
+                this._intervalSubscription = interval(1000).subscribe((count) => this._updateMetrics(count));
+            } else {
+                console.log('no aggregator_address in config file');
+            }
+        });
     }
 
     stop(): void {
         console.log('stop metrics-data service');
+        this._intervalSubscription?.unsubscribe();
+    }
+
+    private _updateMetrics(count: number): void {
+        console.log(count);
     }
 }
